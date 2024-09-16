055de986229ff3396686a704528a2b43ba71649a
sanpielaf <sanpielaf@alu.edu.gva.es>
2024-09-16 13:25:48 +0200

    ejercicio 2 corregido

--- unidad1.bash/ej2.sh
+++ unidad1.bash/ej2.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Nombre del fichero donde guardaremos el listado
 FICHERO="listado_etc.txt"
 
-# Listar archivos y directorios de /etc y guardarlos en el fichero
 ls -l /etc > $FICHERO
 
-# Imprimir por pantalla el contenido del fichero
 cat $FICHERO
 
 echo "El listado se ha guardado en $FICHERO y se ha mostrado por pantalla."
