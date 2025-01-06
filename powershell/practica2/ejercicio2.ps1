8868b2f5a9a3d5e1c10b2f1d077986abc90a5f7a
sanpielaf <sanpielaf@alu.edu.gva.es>
2025-01-06 22:46:21 +0100

    practica 2

---
+++ powershell/practica2/ejercicio2.ps1
@@ -0,0 +1,17 @@
+﻿$num1 = Read-Host "Introduce el primer número"
+$num2 = Read-Host "Introduce el segundo número"
+
+$num1 = [int]$num1
+$num2 = [int]$num2
+
+$suma = $num1 + $num2
+$resta = $num1 - $num2
+$multiplicacion = $num1 * $num2
+$division = $num1 / $num2
+$resto = $num1 % $num2
+
+Write-Host "Suma: $suma"
+Write-Host "Resta: $resta"
+Write-Host "Multiplicación: $multiplicacion"
+Write-Host "División: $division"
+Write-Host "Resto: $resto"
---
+++ powershell/practica2/ejercicio3.ps1
@@ -0,0 +1,9 @@
+﻿$horasTrabajadas = Read-Host "Introduce el número de horas trabajadas"
+$costePorHora = Read-Host "Introduce el coste por hora"
+
+$horasTrabajadas = [decimal]$horasTrabajadas
+$costePorHora = [decimal]$costePorHora
+
+$salario = $horasTrabajadas * $costePorHora
+
+Write-Host "El salario a pagar es: $salario euros"
