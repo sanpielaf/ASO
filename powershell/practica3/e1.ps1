aed17a7d3b8f3f414daaa5a1162bd040394a5266
sanpielaf <sanpielaf@alu.edu.gva.es>
2025-01-06 22:58:19 +0100

    practica 3

---
+++ powershell/practica3/e1.ps1
@@ -0,0 +1,9 @@
+﻿$numero = Read-Host "Introduce un número"
+
+$numero = [int]$numero
+
+if ($numero % 2 -eq 0) {
+    Write-Host "El número $numero es par"
+} else {
+    Write-Host "El número $numero es impar"
+}
