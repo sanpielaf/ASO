b338c1cbb80882d87a1930e54dcbe84b43cbc09f
sanpielaf <sanpielaf@alu.edu.gva.es>
2025-01-14 22:29:56 +0100

    te faltan 20 ejercicios

---
+++ powershell/practica3/e8.ps1
@@ -0,0 +1,20 @@
+﻿$nombre = Read-Host "ingrese su nombre"
+$sexo = Read-Host "ingrese su sexo (M para masculino, F para femenino)"
+
+$primeraLetra = $nombre.Substring(0, 1).ToUpper()
+
+if ($sexo -eq "F") {
+    if ($primeraLetra -lt "M") {
+        $grupo = "A"
+    } else {
+        $grupo = "B"
+    }
+} elseif ($sexo -eq "M") {
+    if ($primeraLetra -gt "N") {
+        $grupo = "A"
+    } else {
+        $grupo = "B"
+    }
+}
+
+Write-Host "Usted pertenece al Grupo $grupo"
