1b0e4461da6d23e507c6b2c76020c17138fd435c
unknown <Administrador@SANTI.LOCAL>
2025-01-15 11:19:05 +0100

    terminar PR3-AD

---
+++ powershell/ut6/pr3.ps1
@@ -0,0 +1,9 @@
+﻿$empleados = Import-Csv -Path "C:\Users\Administrador\Downloads\archivos\empleados.csv" -Delimiter ";"
+$departamentos = Import-Csv -Path "C:\Users\Administrador\Downloads\archivos\departamentos.csv" -Delimiter ";"
+
+
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=santi,DC=local"
+
+foreach ($departamento in $departamentos) {
+    New-ADOrganizationalUnit -Name $departamento.departamento -Path "OU=Empresa,DC=santi,DC=local"
+}
