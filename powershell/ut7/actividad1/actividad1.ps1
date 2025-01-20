e3434fe51c457fe097b85b83288b958c334b8b32
sanpielaf <sanpielaf@alu.edu.gva.es>
2025-01-20 13:53:52 +0100

    hoy terminar la act1 y act2

---
+++ powershell/ut7/actividad1/actividad1.ps1
@@ -0,0 +1,4 @@
+﻿#1-Visualiza las carpetas compartidas, excluyendo los especiales de sistema.Get-SmbShare | Where-Object { $_.Special -eq $false }
+#2-Crea una carpeta con el nombre ASO en el path C:\ASO utilizando el comando New-Item
+New-Item -Path "C:\carpetaASO" -ItemType Directory
+#3-Comparte la carpeta C:\ASO con el nombre aso utilizando el comando New-SmbShareNew-SmbShare -Name "aso" -Path "C:\carpetaASO" -FullAccess "Everyone"
--- powershell/ut7/ejercicio3.ps1
+++
@@ -1,5 +0,0 @@
-﻿$numero = [int] Read-Host "Escribe un número entero positivo"
-
-for ($i = 1, $i -le $numero, $i = $i + 2) {
-    write-host "los numeros impares son: $numero"
-}
